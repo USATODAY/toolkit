613b4cbab8d9de32613557fac9b1154be6536a64
Pim Linders <pimlinders@gmail.com>
2016-04-06 14:05:50 -0400

    working grunt script for deployment

--- Gruntfile.js
+++ Gruntfile.js
@@ -52,47 +52,73 @@ module.exports = function (grunt) {
                 dest: 'tables/static/tables/dist/table_viz/fonts',
                 expand: true,
                 flatten: true
+            },
+            table_viz_component_html: {
+                cwd: 'tables/static/tables/components',
+                src: [
+                    'responsive-table/*.html',
+                    'table-viz/*.html'
+                ],
+                dest: 'tables/static/tables/dist/table_viz/tables/components',
+                expand: true
+            }
+        },
+        fetchpages: {
+            table_viz: {
+                options: {
+                    baseURL: 'http://127.0.0.1:8000/tables/viewer/?use_deployment=true',
+                    destinationFolder: 'tables/static/tables/dist/table_viz/',
+                    urls: [{
+                        url: 'http://127.0.0.1:8000/tables/viewer/?use_deployment=true', localFile: 'index.html'
+                    }]
+                }
+            }
+        },
+        htmlmin: {
+            table_viz_index: {
+                options: {
+                    removeComments: true,
+                    collapseWhitespace: true
+                },
+                files: {
+                    'tables/static/tables/dist/table_viz/index.html': 'tables/static/tables/dist/table_viz/index.html'
+                }
+            }
+        },
+        'ftp-deploy': {
+            table_viz: {
+                auth: {
+                    host: 'usatoday.upload.akamai.com',
+                    port: 21,
+                    authPath: '/Users/plinders/Documents/.ftppass',
+                    authKey: 'experiments'
+                },
+                src: 'tables/static/tables/dist/table_viz/',
+                dest: '/17200/experiments/usatoday/responsive/data-tables',
+                exclusions: []
             }
         }
-        // htmlmin: {
-        //     options: {
-        //         removeComments: true,
-        //         collapseWhitespace: true
-        //     },
-        //     component_html: {
-        //         cwd: 'components',
-        //         src: [
-        //             '**/*.html'
-        //         ],
-        //         dest: 'dist/components',
-        //         expand: true
-        //     },
-        //     index: {
-        //         files: {
-        //             'dist/index.html': 'dist/index.html'
-        //         }
-        //     }
-        // },
-        // 'ftp-deploy': {
-        //     build: {
-        //         auth: {
-        //             host: 'usatoday.upload.akamai.com',
-        //             port: 21,
-        //             authPath: '/Users/plinders/Documents/.ftppass',
-        //             authKey: 'experiments'
-        //         },
-        //         src: 'dist',
-        //         dest: '/17200/experiments/usatoday/responsive/candidate-tax-calculator',
-        //         exclusions: []
-        //     }
-        // }
     });
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
-    // grunt.loadNpmTasks('grunt-contrib-htmlmin');
-    // grunt.loadNpmTasks('grunt-ftp-deploy');
-    grunt.registerTask('table-viz', ['clean:table_viz', 'cssmin:table_viz', 'uglify:table_viz', 'concat:table_viz_libs', 'copy:table_viz_icons']);
+    grunt.loadNpmTasks('grunt-fetch-pages');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-ftp-deploy');
+    grunt.registerTask('table-viz-build', [
+        'clean:table_viz',
+        'cssmin:table_viz',
+        'uglify:table_viz',
+        'concat:table_viz_libs',
+        'copy:table_viz_icons',
+        'copy:table_viz_component_html',
+        'fetchpages:table_viz',
+        'htmlmin:table_viz_index'
+    ]);
+    grunt.registerTask('table-viz-build-deploy', [
+        'table-viz-build',
+        'ftp-deploy:table_viz'
+    ]);
 };
