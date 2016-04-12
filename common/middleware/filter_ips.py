2b9362d8303579e98e7a1c893004a4bae3117ca4
Pim Linders <pimlinders@gmail.com>
2016-04-12 09:55:51 -0400

    get client ip with multiple methods

--- common/middleware/filter_ips.py
+++ common/middleware/filter_ips.py
@@ -4,10 +4,18 @@ from django.conf import settings
 
 class FilterIPMiddleware(object):
 
+    def get_client_ip(self, request):
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[-1].strip()
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+        return ip
+
     # Check if client IP is allowed
     def process_request(self, request):
 
-        ip = request.META.get('REMOTE_ADDR') # Get client IP
+        ip = self.get_client_ip(request)
 
         if ip not in settings.ALLOWED_IPS:
             raise PermissionDenied
